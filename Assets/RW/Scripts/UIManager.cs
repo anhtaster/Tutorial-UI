917d07e060128a65b63a0afc657fda92b01bdd6e
anhtaster <154873647+anhtaster@users.noreply.github.com>
2025-11-10 15:58:13 +0700

    Use DOTween for settings button and settings dialog animtaion

--- Assets/RW/Scripts/UIManager.cs
+++ Assets/RW/Scripts/UIManager.cs
@@ -1,18 +1,51 @@
-using System.Collections;
+﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using DG.Tweening;
 
 public class UIManager : MonoBehaviour
 {
-    public Animator startButton;
-    public Animator settingsButton;
-    public Animator dialog;
+    [Header("Settings Panel (DOTween)")]
+    public RectTransform startButton;
+    public RectTransform settingsButton;
+    public RectTransform dialog;
+
+    public CanvasGroup dialogCanvasGroup;
+
+    [Header("Slide Menu (Animator)")]
     public Animator contentPanel;
     public Animator gearImage;
 
+    [Header("DOTween Animation Settings")]
+    public float animationDuration = 0.5f;
 
+    [Tooltip("Play")]
+    public Vector2 startButtonTargetPos;
 
+    [Tooltip("Settings")]
+    public Vector2 settingsButtonTargetPos;
+
+    [Tooltip("Panel Settings")]
+    public Vector2 dialogTargetPos;
+
+    private Vector2 startButtonOriginalPos;
+    private Vector2 settingsButtonOriginalPos;
+    private Vector2 dialogOriginalPos;
+
+    void Awake()
+    {
+        startButtonOriginalPos = startButton.anchoredPosition;
+        settingsButtonOriginalPos = settingsButton.anchoredPosition;
+        dialogOriginalPos = dialog.anchoredPosition;
+
+        if (dialogCanvasGroup)
+        {
+            dialogCanvasGroup.alpha = 0;
+            dialogCanvasGroup.interactable = false;
+            dialogCanvasGroup.blocksRaycasts = false;
+        }
+    }
 
     public void StartGame()
     {
@@ -21,25 +54,29 @@ public class UIManager : MonoBehaviour
 
     public void OpenSettings()
     {
-        startButton.SetBool("isHidden", true);
-        settingsButton.SetBool("isHidden", true);
-        dialog.SetBool("isHidden", false);
+        startButton.DOAnchorPos(startButtonTargetPos, animationDuration).SetEase(Ease.OutBack);
+        settingsButton.DOAnchorPos(settingsButtonTargetPos, animationDuration).SetEase(Ease.OutBack);
+
+        dialog.DOAnchorPos(dialogTargetPos, animationDuration).SetEase(Ease.OutBack);
+        dialogCanvasGroup.DOFade(1, animationDuration);
+        dialogCanvasGroup.interactable = true;
+        dialogCanvasGroup.blocksRaycasts = true;
     }
 
     public void CloseSettings()
     {
-        startButton.SetBool("isHidden", false);
-        settingsButton.SetBool("isHidden", false);
-        dialog.SetBool("isHidden", true);
-    }
+        startButton.DOAnchorPos(startButtonOriginalPos, animationDuration).SetEase(Ease.InBack);
+        settingsButton.DOAnchorPos(settingsButtonOriginalPos, animationDuration).SetEase(Ease.InBack);
 
+        dialog.DOAnchorPos(dialogOriginalPos, animationDuration).SetEase(Ease.InBack);
+        dialogCanvasGroup.DOFade(0, animationDuration);
+        dialogCanvasGroup.interactable = false;
+        dialogCanvasGroup.blocksRaycasts = false;
+    }
     public void ToggleMenu()
     {
         bool isHidden = contentPanel.GetBool("isHidden");
         contentPanel.SetBool("isHidden", !isHidden);
         gearImage.SetBool("isHidden", !isHidden);
     }
-
-
-
 }
